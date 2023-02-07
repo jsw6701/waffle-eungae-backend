cb0237eac47dec5792d2c1e0b397a7a86e8871ae
jsw6701 <anes53027@gmail.com>
2023-02-08 01:36:08 +0900

    init commit

---
+++ src/main/java/com/example/waffleeungaebackend/WaffleEungaeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.waffleeungaebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaffleEungaeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaffleEungaeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/waffleeungaebackend/WaffleEungaeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waffleeungaebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaffleEungaeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
