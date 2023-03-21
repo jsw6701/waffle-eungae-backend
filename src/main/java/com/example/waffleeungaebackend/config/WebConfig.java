3711eff4892b3cf042742da8487f1536bf04f2cf
jeonsangmin <jsangmin99@gmail.com>
2023-03-22 00:56:41 +0900

    jar

--- src/main/java/com/example/waffleeungaebackend/config/WebConfig.java
+++ src/main/java/com/example/waffleeungaebackend/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://waffle-eungae-frontend.vercel.app","http://localhost:3000")
+                .allowedOriginPatterns("https://waffle-eungae-frontend.vercel.app","http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
