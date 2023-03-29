aa60e05a393259c35115deffcb05e738ebe09fc6
jsw6701 <anes53027@gmail.com>
2023-03-29 22:36:22 +0900

    멤버 GET API 생성 ( 로그인 유저 email, id ) + jar

---
+++ src/main/java/com/example/waffleeungaebackend/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.example.waffleeungaebackend.controller;
+
+import com.example.waffleeungaebackend.config.login.LoginUser;
+import com.example.waffleeungaebackend.dto.MemberDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@AllArgsConstructor
+@RestController
+public class MemberController {
+
+    @GetMapping("/loginId")
+    public ResponseEntity<Long> getName(@ApiIgnore @LoginUser MemberDto memberDto){
+        return ResponseEntity.ok(memberDto.getMemberId());
+    }
+
+
+    @GetMapping("/loginEmail")
+    public ResponseEntity<String> getEmail(@ApiIgnore @LoginUser MemberDto memberDto){
+        return ResponseEntity.ok(memberDto.getEmail());
+    }
+}
